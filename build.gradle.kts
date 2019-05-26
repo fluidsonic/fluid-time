c555224d603ec0e4c8bf25e873f801c48dfe5664
Marc Knaup <marc@knaup.io>
2019-05-26 11:58:17 +0100

    added Gradle tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import com.github.fluidsonic.fluid.library.*
+import org.jetbrains.kotlin.gradle.plugin.mpp.*
 
 plugins {
 	id("com.github.fluidsonic.fluid-library") version "0.9.13"
@@ -16,9 +17,19 @@ fluidLibraryVariant {
 
 kotlin {
 	iosX64()
+	macosX64()
 
 	sourceSets {
 		getByName("iosX64Main") {
+			kotlin.setSrcDirs(listOf("sources/ios"))
+			resources.setSrcDirs(emptyList<Any>())
+
+			dependencies {
+				implementation(kotlinx("serialization-runtime-native", "0.11.0"))
+			}
+		}
+
+		getByName("macosX64Main") {
 			kotlin.setSrcDirs(listOf("sources/ios"))
 			resources.setSrcDirs(emptyList<Any>())
 
@@ -37,6 +48,35 @@ kotlin {
 			dependencies {
 				implementation("org.threeten:threetenbp:1.4.0")
 			}
+		}
+	}
+}
+
+
+tasks.create("macosTest") {
+	dependsOn("linkTestDebugExecutableMacosX64")
+	group = JavaBasePlugin.VERIFICATION_GROUP
+
+	doLast {
+		val binary = kotlin.targets.getByName<KotlinNativeTarget>("macosX64").binaries.getExecutable("test", "DEBUG").outputFile
+		exec {
+			println("$ \"${binary.absolutePath}\"")
+			commandLine(binary.absolutePath)
+		}
+	}
+}
+
+
+tasks.create("iosTest") {
+	val device = findProperty("iosDevice")?.toString() ?: "iPhone 8"
+	dependsOn("linkTestDebugExecutableIosX64")
+	group = JavaBasePlugin.VERIFICATION_GROUP
+
+	doLast {
+		val binary = kotlin.targets.getByName<KotlinNativeTarget>("iosX64").binaries.getExecutable("test", "DEBUG").outputFile
+		exec {
+			println("$ xcrun simctl spawn \"$device\" \"${binary.absolutePath}\"")
+			commandLine("xcrun", "simctl", "spawn", device, binary.absolutePath)
 		}
 	}
 }
