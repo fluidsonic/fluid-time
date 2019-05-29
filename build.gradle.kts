5ee501c589c7c41d2232eb171a681cffc9a52807
Marc Knaup <marc@knaup.io>
2019-05-29 17:41:41 +0100

    added iOS device support

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 import com.github.fluidsonic.fluid.library.*
-import org.jetbrains.kotlin.gradle.plugin.mpp.*
 
 plugins {
-	id("com.github.fluidsonic.fluid-library") version "0.9.13"
+	id("com.github.fluidsonic.fluid-library") version "0.9.14"
 }
 
 fluidLibrary {
@@ -12,32 +11,14 @@ fluidLibrary {
 
 fluidLibraryVariant {
 	description = "multiplatform date & time library"
-	jdk = JDK.v1_7
+
+	jvm(JDK.v1_7)
+	objc()
 }
 
+
 kotlin {
-	iosX64()
-	macosX64()
-
 	sourceSets {
-		getByName("iosX64Main") {
-			kotlin.setSrcDirs(listOf("sources/ios"))
-			resources.setSrcDirs(emptyList<Any>())
-
-			dependencies {
-				implementation(kotlinx("serialization-runtime-native", "0.11.0"))
-			}
-		}
-
-		getByName("macosX64Main") {
-			kotlin.setSrcDirs(listOf("sources/ios"))
-			resources.setSrcDirs(emptyList<Any>())
-
-			dependencies {
-				implementation(kotlinx("serialization-runtime-native", "0.11.0"))
-			}
-		}
-
 		commonMain {
 			dependencies {
 				implementation(kotlinx("serialization-runtime", "0.11.0"))
@@ -49,39 +30,11 @@ kotlin {
 				implementation("org.threeten:threetenbp:1.4.0")
 			}
 		}
-	}
-}
 
-
-val macosTest by tasks.creating<Task> {
-	dependsOn("linkTestDebugExecutableMacosX64")
-	group = JavaBasePlugin.VERIFICATION_GROUP
-
-	doLast {
-		val binary = kotlin.targets.getByName<KotlinNativeTarget>("macosX64").binaries.getExecutable("test", "DEBUG").outputFile
-		exec {
-			println("$ \"${binary.absolutePath}\"")
-			commandLine(binary.absolutePath)
+		objcMain {
+			dependencies {
+				implementation(kotlinx("serialization-runtime-native", "0.11.0"))
+			}
 		}
 	}
-}
-
-
-val iosTest by tasks.creating<Task> {
-	val device = findProperty("iosDevice")?.toString() ?: "iPhone 8"
-	dependsOn("linkTestDebugExecutableIosX64")
-	group = JavaBasePlugin.VERIFICATION_GROUP
-
-	doLast {
-		val binary = kotlin.targets.getByName<KotlinNativeTarget>("iosX64").binaries.getExecutable("test", "DEBUG").outputFile
-		exec {
-			println("$ xcrun simctl spawn \"$device\" \"${binary.absolutePath}\"")
-			commandLine("xcrun", "simctl", "spawn", device, binary.absolutePath)
-		}
-	}
-}
-
-tasks.named("check") {
-	dependsOn("iosTest")
-	dependsOn("macosTest")
 }
