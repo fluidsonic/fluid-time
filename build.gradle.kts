767f0673296de7ed04ce14bc9915df7490cfc7d9
Marc Knaup <marc@knaup.io>
2020-01-24 17:21:00 +0700

    🎉 0.9.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.fluidsonic.gradle") version "1.0.6"
 }
 
-fluidLibrary(name = "time", version = "0.9.16")
+fluidLibrary(name = "time", version = "0.9.17")
 
 fluidLibraryVariant {
 	description = "Multiplatform date & time library"
