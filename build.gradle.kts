07384fe1b71358c8b66a12b6d98397eda684e779
Marc Knaup <marc@knaup.io>
2021-01-17 23:38:24 +0100

    fluid-gradle 1.1.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import io.fluidsonic.gradle.*
 
 plugins {
-	id("io.fluidsonic.gradle") version "1.1.16"
+	id("io.fluidsonic.gradle") version "1.1.17"
 }
 
 fluidLibrary(name = "time", version = "0.12.0")
