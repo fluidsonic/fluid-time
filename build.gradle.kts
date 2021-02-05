becae053595e7543ade9f30e5fc490824e56e472
Marc Knaup <marc@knaup.io>
2021-02-05 01:43:56 +0100

    🎉 0.13.2-kotlin-1.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 	id("io.fluidsonic.gradle") version "1.1.18"
 }
 
-fluidLibrary(name = "time", version = "0.13.1")
+fluidLibrary(name = "time", version = "0.13.2-kotlin-1.5")
 
 fluidLibraryModule(description = "Kotlin multiplatform date & time library") {
 	language {
